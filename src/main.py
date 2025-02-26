6dad8ad7229e123d3784db78eec8e216a99100f7
MsMauve <zoetropesilver@gmail.com>
2025-02-26 13:33:43 -0700

    added textnode.py and initialized main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(text_node)
+
+main()
