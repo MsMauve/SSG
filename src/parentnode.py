ed7da5fae5b28fe8c178747f6ff81affd1922bd1
MsMauve <zoetropesilver@gmail.com>
2025-02-27 11:11:51 -0700

    added ParentNode and tests

---
+++ src/parentnode.py
@@ -0,0 +1,25 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+        self.children = children
+        
+    def to_html(self):
+        child_html = ""
+        if self.tag is None:
+            raise ValueError("Instance must contain a tag")
+        if self.children is None:
+            raise ValueError("Object has no children")
+        else:
+            for child in self.children:
+               child_html = child_html + child.to_html()
+            return f"<{self.tag}>{child_html}</{self.tag}>"
+        
+    def props_to_html(self):
+        finished_html = ""
+        if self.props:
+            for key, value in self.props.items():
+                finished_html += f' {key}="{value}"'
+        return finished_html
+        
